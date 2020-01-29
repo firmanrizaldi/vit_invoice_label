04bae995d08deb3f43f83975c6576e8b56ac88dc
firmanrizaldi <firmanrizaldiyusup@gmail.com>
2020-01-29 11:53:05 +0700

    hapus yang ga penting

--- __manifest__.py
+++ __manifest__.py
@@ -25,9 +25,7 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        # 'views/views.xml',
-        # 'views/templates.xml',
+       
         'report/invoice.xml',
     ],
     "application": True,
